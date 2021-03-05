09bf52b5c262c9bf27d6a06aec5e30946226f229
Michael Hall <hall@michaelhall.se>
2021-03-05 14:11:01 +0100

    Add strict types declarations

--- src/PhoneNumber.php
+++ src/PhoneNumber.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Savea\PhoneNumber;
 
 use Savea\PhoneNumber\CountryHandlers\CountryHandlerInterface;
--- tests/PhoneNumberTest.php
+++ tests/PhoneNumberTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Savea\PhoneNumber\Tests;
 
 use PHPUnit\Framework\TestCase;
